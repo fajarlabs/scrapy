f71fea9cb4e71373552dfbd9cc5e6d9edb886d96
fajarrdp <fajarrdp@gmail.com>
2018-08-13 15:02:23 +0700

    Update

--- tutorial/tutorial/spiders/detik.py
+++ tutorial/tutorial/spiders/detik.py
@@ -1,11 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from scrapy.selector import Selector
+from scrapy.http import HtmlResponse
+from scrapy import Request
+
+# import log module
+import logging
+# config module
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
 
 
 class DetikSpider(scrapy.Spider):
     name = 'detik'
     allowed_domains = ['detik.com']
-    start_urls = ['https://detik.com/']
-
+    start_urls = ['https://www.detik.com/search/searchall?query=kapal+tenggelam']
+    
     def parse(self, response):
-        pass
+        # get html full page
+        #logging.info(response.text)
+
+        # get href link form query results
+        logging.info(response.selector.xpath('//article//a/@href').extract())
