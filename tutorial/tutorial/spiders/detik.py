7062eb7dab3baea47602f3c1e75ce8b96cf886ad
fajarrdp <fajarrdp@gmail.com>
2018-08-13 15:32:44 +0700

    Update

--- tutorial/tutorial/spiders/detik.py
+++ tutorial/tutorial/spiders/detik.py
@@ -14,10 +14,23 @@ class DetikSpider(scrapy.Spider):
     name = 'detik'
     allowed_domains = ['detik.com']
     start_urls = ['https://www.detik.com/search/searchall?query=kapal+tenggelam']
-    
+    dont_filter=True
+
+    # for parse first
     def parse(self, response):
         # get html full page
         #logging.info(response.text)
 
         # get href link form query results
-        logging.info(response.selector.xpath('//article//a/@href').extract())
+        link_href = response.selector.xpath('//article//a/@href').extract()
+        for href in link_href :
+            logging.info(href)
+            yield scrapy.Request(href, callback=self.parse_detail, dont_filter=True)
+        # do something for recursives
+
+    # for parse detail news
+    # get content article
+    def parse_detail(self, response):
+        link_href = response.selector.xpath("//article//div//div[contains(@id, 'detikdetailtext')]/text()").extract()
+        # show to log
+        logging.info(link_href)
